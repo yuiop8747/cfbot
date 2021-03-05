f5913926114ffa8c09afc2c767a616dbbd7a0d8f
Lenick <lenick@MacBook-Pro.local>
2021-03-06 07:48:36 +0900

    수정 : 주석삭제

--- request.js
+++ request.js
@@ -8,19 +8,13 @@ const getCfInfo = async () => {
       const $bodyList = $(
         ".contestList > .datatable > div > table > tbody"
       ).children("tr");
-      //   console.log($bodyList.length);
       $bodyList.each((i, elem) => {
-        // console.log($(elem).find("[target=_blank]").text());
-        // console.log($(elem).html());
-        // console.log($(elem).find("td").first().empty().html());
         data.push({
           title: $(elem).find("td").first().text().trim(),
           time: $(elem).find("[target=_blank]").text().trim(),
         });
-        // data.push($(elem).find("[target=_blank]").text().trim());
       });
       for (let i = 1; i < data.length; i += 1) {
-        // console.log(data[i]);
         let str = data[i].time;
         str = str.split(" ");
         let sub = str[1].split(":");
@@ -31,11 +25,9 @@ const getCfInfo = async () => {
         let title = data[i].title.split("\n");
         data[i].title = title[0];
       }
-      //   console.log("request : " + data[1].title);
       resolve(data);
     });
   });
 };
 
 module.exports = getCfInfo;
-// writer , title , 대회시간 정보 필요
